bc4bb71a1775030e1f8d70fe36ed0d33df3260b9
matdev90 <matdev90@localhost.localdomain>
2022-07-12 20:56:01 +0800

    merapikan source code

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:bmi/bmi_data_screen.dart';
+import 'package:bmi/constants/const.dart';
+import 'package:bmi/view/bmi_data_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,18 +14,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
+        title: 'BMI App',
         theme: ThemeData(
-          // This is the theme of your application.
-          //
-          // Try running your application with "flutter run". You'll see the
-          // application has a blue toolbar. Then, without quitting the app, try
-          // changing the primarySwatch below to Colors.green and then invoke
-          // "hot reload" (press "r" in the console where you ran "flutter run",
-          // or simply save your changes to "hot reload" in a Flutter IDE).
-          // Notice that the counter didn't reset back to zero; the application
-          // is not restarted.
-          primarySwatch: Colors.blueGrey,
+          scaffoldBackgroundColor: primaryColor,
+          primaryColor: primaryColor,
+          appBarTheme: AppBarTheme(
+            backgroundColor: primaryColor,
+          ),
+          primarySwatch: Colors.blue,
         ),
         home: BmiDataScreen());
   }
---
+++ lib/view/bmi_data_screen.dart
@@ -0,0 +1,169 @@
+import 'package:bmi/constants/const.dart';
+import 'package:bmi/view/bmi_result_screen.dart';
+import 'package:flutter/material.dart';
+
+class BmiDataScreen extends StatefulWidget {
+  const BmiDataScreen({Key? key}) : super(key: key);
+
+  @override
+  State<BmiDataScreen> createState() => _BmiDataScreenState();
+}
+
+class _BmiDataScreenState extends State<BmiDataScreen> {
+  int height = 100;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff0a0e21),
+      appBar: AppBar(
+        title: Center(child: Text("BMI Calculator")),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Row(
+              children: const [
+                Expanded(
+                  child: BmiCard(
+                    child: GenderIconText(
+                      icon: Icons.male,
+                      title: 'Male',
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: BmiCard(
+                    child: GenderIconText(
+                      title: 'Female',
+                      icon: Icons.female,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              child: BmiCard(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "HEIGHT",
+                      style: labelTexstyle!.copyWith(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Text(
+                          "182",
+                          style: TextStyle(
+                            fontSize: 50,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                        Text(
+                          "$height",
+                          style: labelTexstyle,
+                        )
+                      ],
+                    ),
+                    Slider(
+                      value: height.toDouble(),
+                      min: 80,
+                      max: 200,
+                      thumbColor: Colors.red,
+                      activeColor: Colors.white,
+                      onChanged: (value) {
+                        height = value.toInt();
+                        setState(() {});
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+              child: Container(
+            color: Colors.green,
+          )),
+          GestureDetector(
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: ((context) {
+                    return BmiResultScreen();
+                  }),
+                ),
+              );
+            },
+            child: Container(
+              height: 60,
+              color: Colors.blue,
+              child: const Center(child: Text("Hitung BMI")),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class BmiCard extends StatelessWidget {
+  const BmiCard({
+    Key? key,
+    this.child,
+  }) : super(key: key);
+
+  final Widget? child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Color(0xff272A4E),
+        borderRadius: BorderRadius.circular(15),
+      ),
+      margin: EdgeInsets.all(15),
+      child: child,
+    );
+  }
+}
+
+class GenderIconText extends StatelessWidget {
+  const GenderIconText({
+    Key? key,
+    required this.title,
+    required this.icon,
+  }) : super(key: key);
+
+  final String title;
+  final IconData icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          icon,
+          size: 80,
+        ),
+        const SizedBox(
+          height: 15,
+        ),
+        Text(
+          title,
+          style: labelTexstyle,
+        ),
+      ],
+    );
+  }
+}
